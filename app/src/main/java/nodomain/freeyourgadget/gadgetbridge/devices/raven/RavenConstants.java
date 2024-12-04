a8d56c00274e66ff084e9cb131736b4fcf7fa870
Lucas Merritt <merrittlj@protonmail.com>
2024-12-03 22:07:00 -0700

    Basic raven support

---
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/raven/RavenConstants.java
@@ -0,0 +1,9 @@
+package nodomain.freeyourgadget.gadgetbridge.devices.raven;
+
+import java.util.UUID;
+
+public final class RavenConstants {
+    public static final UUID UUID_SERVICE_TIME = UUID.fromString("9959a5e0-b1de-11ef-be87-0800200c9a66");
+    public static final UUID UUID_CHARACTERISTIC_TIME_FORMAT = UUID.fromString("9959a5e1-b1de-11ef-be87-0800200c9a66");
+    public static final UUID UUID_CHARACTERISTIC_TIME_SYNC = UUID.fromString("9959a5e2-b1de-11ef-be87-0800200c9a66");
+}
