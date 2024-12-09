41d77474ef41e83064f68a140e1dbf43662e5bec
Lucas Merritt <merrittlj@protonmail.com>
2024-12-09 15:46:19 -0700

    minor notification updates

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/raven/RavenConstants.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/raven/RavenConstants.java
@@ -3,7 +3,7 @@ package nodomain.freeyourgadget.gadgetbridge.devices.raven;
 import java.util.UUID;
 
 public final class RavenConstants {
-    public static final UUID UUID_SERVICE_TIME = UUID.fromString("9959a5e0-b1de-11ef-be87-0800200c9a66");
-    public static final UUID UUID_CHARACTERISTIC_TIME_FORMAT = UUID.fromString("9959a5e1-b1de-11ef-be87-0800200c9a66");
-    public static final UUID UUID_CHARACTERISTIC_TIME_SYNC = UUID.fromString("9959a5e2-b1de-11ef-be87-0800200c9a66");
+    public static final UUID UUID_SERVICE_NOTIF = UUID.fromString("9959a5e0-b1de-11ef-be87-0800200c9a66");
+    public static final UUID UUID_CHARACTERISTIC_NOTIF_APP = UUID.fromString("9959a5e1-b1de-11ef-be87-0800200c9a66");
+    public static final UUID UUID_CHARACTERISTIC_NOTIF_TITLE = UUID.fromString("9959a5e2-b1de-11ef-be87-0800200c9a66");
 }
