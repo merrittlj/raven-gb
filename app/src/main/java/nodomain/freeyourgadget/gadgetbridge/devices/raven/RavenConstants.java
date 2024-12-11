979c737dc54cbad195d60353a49ca1b184b95526
Lucas Merritt <merrittlj@protonmail.com>
2024-12-10 19:22:47 -0700

    Simple notification support

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/raven/RavenConstants.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/raven/RavenConstants.java
@@ -7,4 +7,5 @@ public final class RavenConstants {
     public static final UUID UUID_CHARACTERISTIC_NOTIFY_SOURCE = UUID.fromString("684a4961-b6a6-11ef-be87-0800200c9a66");
     public static final UUID UUID_CHARACTERISTIC_NOTIFY_TITLE = UUID.fromString("684a4962-b6a6-11ef-be87-0800200c9a66");
     public static final UUID UUID_CHARACTERISTIC_NOTIFY_BODY = UUID.fromString("684a4963-b6a6-11ef-be87-0800200c9a66");
+    public static final UUID UUID_CHARACTERISTIC_NOTIFY_SEND = UUID.fromString("684a4964-b6a6-11ef-be87-0800200c9a66");
 }
